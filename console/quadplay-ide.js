6340aa39ce00845c89b07a6f56c7184c7be4229e
Morgan <morgan3d@gmail.com>
2020-05-17 13:06:52 -0400

    Optimized performance on Safari

--- console/quadplay-ide.js
+++ console/quadplay-ide.js
@@ -7,7 +7,7 @@ const deployed = true;
 // Set to true to allow editing of quad://example/ files when developing quadplay
 const ALLOW_EDITING_EXAMPLES = false;
 
-const version  = '2020.05.17.11'
+const version  = '2020.05.17.13'
 const launcherURL = 'quad://console/launcher';
 
 // Token that must be passed to the server to make POST calls
