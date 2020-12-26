ba11d8446809fa63839f4905f0b94dc8b058a665
Morgan <morgan3d@gmail.com>
2020-12-26 15:56:22 -0500

    Scrolling on new host dialog

--- console/quadplay-ide.js
+++ console/quadplay-ide.js
@@ -3,7 +3,7 @@
 
 // Set to false when working on quadplay itself
 const deployed = true;
-const version  = '2020.12.20.09';
+const version  = '2020.12.26.16';
 
 // Set to true to allow editing of quad://example/ files when developing quadplay
 const ALLOW_EDITING_EXAMPLES = ! deployed;
