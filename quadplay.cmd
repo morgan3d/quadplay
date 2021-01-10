88e1e7998d8d8e90dccc9216ba36f887f85a5f68
Kincaid Savoie <kincaidsavoie@gmail.com>
2021-01-10 12:51:05 -0700

    Prefer using py.exe Python launcher on Windows
    On Windows, Python installations come with a launcher called "py.exe"
    by default. This launcher locates Python interpreters and allows users
    to specify whether to use Python 2 or 3 when running scripts. It's
    generally more robust than launching "python.exe" directly.
    https://www.python.org/dev/peps/pep-0397/ has more details.
    
    This commit modifies "quadplay.cmd" to check if "py.exe" is present in
    the user's path, and uses it to run the quadplay Python script if it
    is. This accomplishes two things:
    - it allows users with both Python 2 and 3 installations to launch
      quadplay
    - it avoids an issue where quadplay.cmd accidentally runs the Windows
      Store python.exe shim and silently exits

--- quadplay.cmd
+++ quadplay.cmd
@@ -1,2 +1,10 @@
-@rem Python 3 is called "python" on Windows(!)
-@python quadplay %*
+@rem Check if "py.exe" launcher is installed on Windows. Prefer using that (the
+@rem python executable might be a shim that opens the Windows store)
+
+where.exe "py.exe"
+if %ERRORLEVEL% EQU 0 (
+    @py -3 quadplay %*
+) else (
+    @python quadplay %*
+)
+
