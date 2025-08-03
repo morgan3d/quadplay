7471b5fb550748d767936fda798b4b59ffd0d6ba
Morgan <3970018+morgan3d@users.noreply.github.com>
2025-08-03 00:56:20 -0700

    export

--- console/quadplay-runtime-cpu.js
+++ console/quadplay-runtime-cpu.js
@@ -9824,7 +9824,7 @@ function resized(str, n, pad, right_align) {
 
 function split(str, c, n) {
     if (typeof str !== 'string') {
-        error('split(str) requires a string');
+        $error('split(str) requires a string');
     } else if (c === undefined || c === '' || (is_number(c) && c <= 0)) {
         
         return Array.from(str);
